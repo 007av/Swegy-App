03dac69320045cd6e4fd59f08c31d354ca508312
Avdhut Patole <avdhutpatole5556@gmail.com>
2026-05-08 20:41:08 +0530

    Lying Fundamentals

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 
-const parent = React.createElement("div", {
-    id: "parent"
-},[
-    React.createElement("h1", {key: "h1"}, "Im h1 tag"),
-    React.createElement("h2", {key: "h2"}, "Im h2 tag")
-]);
+const heading = <h1 id="heading">Namsete React 🚀</h1>
+
+const HeaderComponent = ()=> (<h1>Namste from Component 1</h1>) 
+
+const HederComponent2 = ()=>{
+    return (
+        <div>
+            <HeaderComponent />
+            <h1>Namste from Component 2</h1>
+            
+        </div>
+    )
+    
+    
+}
 
 const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HederComponent2 />);
