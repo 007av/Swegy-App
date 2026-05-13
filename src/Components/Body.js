1295b0ceaffc888397ca0f16ebf156e7ca7438e9
Avdhut Patole <avdhutpatole5556@gmail.com>
2026-05-13 15:10:05 +0530

    Added Filtered Restorant for Top rated restorant

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,16 +1,23 @@
 import restoList from "../utils/RESTRO_LIST.JS";
 import RestroCards from "./RestroCards";
+import { useState } from "react";
 
 const Body = () => {
+
+  const [listofRestorant, setListofRestorant] = useState(restoList);
+
+  // const {avgRatingString} = restorant?.card.card.info;
+
   return (
     <div className="body">
       <div className="Search">
         <input placeholder="Food,Restorant,etc"></input>
         <button>Search</button>
       </div>
+      <button className="Top-rated" onClick={()=>{setListofRestorant(listofRestorant.filter((restorant=> restorant.card.card.info.avgRatingString > 4.1))) }}>Top Rated Restorants</button>
       <div className="res-container">
        {
-        restoList.map((resto)=>{
+        listofRestorant.map((resto)=>{
           return(
             <RestroCards key={resto.card.card.info.id} restData={resto}/>
           )
